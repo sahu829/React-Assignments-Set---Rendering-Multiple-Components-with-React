040b0d158fe65e1ba2c806983a81bcb2bce8c7b0
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-11-30 08:26:14 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">project-name</h1>
+      <h1 data-ns-test="project-description">project-name krishna</h1>
+    </div>
+  );
+};
 
 export default App;
